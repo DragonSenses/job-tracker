be3cae83ee34c3c63345b173d2d4cfcc082df84b
DragonSenses <kendrickfrancisco@gmail.com>
2023-03-05 11:55:14 -0800

    Refactor Logo component

---
+++ client/src/components/Logo.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import logo from '../assets/images/logo.svg'
+
+export default function Logo() {
+  return (
+    <img src={logo} alt="job tracker logo" className="logo" />
+  )
+}
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import logo from '../assets/images/logo.svg'
 import main from '../assets/images/main.svg'
 import Wrapper from '../assets/wrappers/LandingPage';
+import Logo from '../components/Logo';
 
 function Landing() {
   return (
     <Wrapper>
       <nav>
-        <img src={logo} alt="job tracker logo" className="logo" />
+        <Logo />
       </nav>
       <div className="container page">
 
