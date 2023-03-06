be435333669123f00bac4b828fd46983e4845ecb
DragonSenses <kendrickfrancisco@gmail.com>
2023-03-05 17:20:53 -0800

    router components added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import Landing from "./pages/Landing"
+import { BrowserRouter, Routes, Route, Links } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Landing />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Dashboard</div>}/>
+        <Route path="/register" element={<div>Register</div>}/>     
+        <Route path="/landing" element={<Landing />}/>     
+        <Route path="*" element={<h1>Error</h1>}/>     
+      </Routes>
+    </BrowserRouter>
   );
 }
 
