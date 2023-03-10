71a40571a444add8c8784c87f7d506b866e47c8e
DragonSenses <kendrickfrancisco@gmail.com>
2023-03-10 15:16:28 -0800

    Create jobsController.js

---
+++ controllers/jobsController.js
@@ -0,0 +1,20 @@
+const createJob = async (req, res) => {
+  res.send('createJob');
+}
+
+const getAllJobs = async (req, res) => {
+  res.send('getAllJobs');
+}
+
+const updateJob = async (req, res) => {
+  res.send('updateJob');
+}
+
+const deleteJob = async (req, res) => {
+  res.send('deleteJob');
+}
+const showStats = async (req, res) => {
+  res.send('showStats');
+}
+
+export { createJob, getAllJobs, updateJob, deleteJob, showStats }
