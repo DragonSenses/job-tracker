fce3ebd981079f210e7581aef35d398009424e83
DragonSenses <kendrickfrancisco@gmail.com>
2023-03-10 13:45:41 -0800

    Set up database connection

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ dotenv.config()
 import express from 'express'
 const app = express()
 
+import connectDB from './db/connect.js'
+
 import notFoundMiddleware from './middleware/not-found.js'
 import errorHandlerMiddleware from './middleware/error-handler.js'
 
@@ -11,11 +13,21 @@ app.get('/', (req, res) => {
   res.send('Hello');
 })
 
-
-
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 4000;
 
-app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+const start = async () => {
+  try{
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => { 
+      console.log(`Server is listening on port ${port}...`)
+    });
+
+  } catch(error){
+    console.log(error);
+  }
+}
+
+start();
