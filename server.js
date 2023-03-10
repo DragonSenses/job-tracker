ac2e0fb2dddf03f2f6fb8ba955dd3beb33b671c6
DragonSenses <kendrickfrancisco@gmail.com>
2023-03-10 14:29:33 -0800

    Create Authentication Routes

--- server.js
+++ server.js
@@ -4,14 +4,23 @@ dotenv.config()
 import express from 'express'
 const app = express()
 
+// Database and AuthenticateUser
 import connectDB from './db/connect.js'
 
+// Routers
+import authRouter from './routes/authRoutes.js'
+
+// Middleware
 import notFoundMiddleware from './middleware/not-found.js'
 import errorHandlerMiddleware from './middleware/error-handler.js'
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello');
 })
+
+app.use('/api/v1/auth', authRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
