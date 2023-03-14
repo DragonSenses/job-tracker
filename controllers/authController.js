97523d310b5cd2a74100dd6b16659b670c3a0ea0
DragonSenses <kendrickfrancisco@gmail.com>
2023-03-13 23:08:38 -0700

    Use http-status-codes to increase readability

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,9 @@
 import User from '../models/User.js';
+import { StatusCodes } from 'http-status-codes';
 
 const register = async (req, res) => {
   const user = await User.create(req.body);
-  res.status(201).json({user});
+  res.status(StatusCodes.CREATED).json({user});
 }
 
 const login = (req, res) => {
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,14 @@
+import { StatusCodes } from 'http-status-codes';
+
 const errorHandlerMiddleware = (err, req, res, next) => {
-  res.status(500).json({msg: err});
+  console.log(err);
+
+  const defaultError = {
+    statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: '500 - Something went wrong, try again later',
+  }
+
+  res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg: err});
 }
 
 export default errorHandlerMiddleware
