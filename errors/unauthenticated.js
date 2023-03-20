67765e12bd37970c13469b08a7a5fb8b1809ab30
DragonSenses <kendrickfrancisco@gmail.com>
2023-03-20 14:56:16 -0700

    Fix module import

--- errors/unauthenticated.js
+++ errors/unauthenticated.js
@@ -1,5 +1,5 @@
 import { StatusCodes } from "http-status-codes";
-import CustomAPIError from "./custom-api";
+import CustomAPIError from "./custom-api.js";
 
 class UnAuthenticatedError extends CustomAPIError {
   constructor(messsage){
