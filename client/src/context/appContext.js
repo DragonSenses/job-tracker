d87e5933c62c0b85f68d64060b65402ebd4db406
DragonSenses <kendrickfrancisco@gmail.com>
2023-03-21 00:41:30 -0700

    Update loginUser function

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -83,7 +83,24 @@ export default function AppProvider(props) {
   };
 
   const loginUser = async (currentUser) => {
-    console.log(currentUser);
+    dispatch({ type: LOGIN_USER_BEGIN });
+    try{
+      const { data } = await axios.post('/api/v1/auth/login', currentUser);
+      const { user, token, location } = data;
+
+      dispatch({
+        type: LOGIN_USER_SUCCESS,
+        payload: { user, token, location },
+      });
+      
+      addUserToLocalStorage({ user, token, location });
+    } catch(error){
+      dispatch( {
+        type: LOGIN_USER_ERROR,
+        payload: { msg: error.response.data.msg },
+      })
+    }
+    clearAlert();
   };
 
   return (
