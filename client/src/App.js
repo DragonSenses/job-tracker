9a2d460f120fe2f971550da049dfd9b4bb532fea
DragonSenses <kendrickfrancisco@gmail.com>
2023-03-22 05:16:33 -0700

    Create the index page

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/">
-          <Route path="stats" element={<Stats/>}/>
+        <Route path="/" element={<SharedLayout/>}>
+          <Route index element={<Stats/>}/>
           <Route path="all-jobs" element={<AllJobs/>}/>
           <Route path="add-job" element={<AddJob/>}/>
           <Route path="profile" element={<Profile/>}/>
