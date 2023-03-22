fdaa6362c12c1820ff6d31a7e6ae010350d7a443
DragonSenses <kendrickfrancisco@gmail.com>
2023-03-22 04:13:06 -0700

    Fix imports

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,5 +1,5 @@
-import AddJob from 'AddJob.js';
-import AllJobs from 'AllJobs.js';
+import AddJob from './AddJob.js';
+import AllJobs from './AllJobs.js';
 import Profile from './Profile.js';
 import SharedLayout from './SharedLayout.js';
 import Stats from './Stats.js';
