f75c80edbfb7ead9d979e5793cd3a7a8a4d59c01
DragonSenses <kendrickfrancisco@gmail.com>
2023-03-22 18:58:03 -0700

    Create LargeSidebar

---
+++ client/src/components/LargeSidebar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Wrapper from '../assets/wrappers/LargeSidebar.js';
+
+export default function LargeSidebar() {
+  return (
+    <Wrapper>
+      LargeSidebar
+    </Wrapper>
+  )
+}
