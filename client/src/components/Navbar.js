0cb5a14b4a795fdf24c30b330ec250fc789128d9
DragonSenses <kendrickfrancisco@gmail.com>
2023-03-22 18:58:13 -0700

    Create Navbar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Wrapper from '../assets/wrappers/Navbar.js';
+
+export default function Navbar() {
+  return (
+    <Wrapper>
+      Navbar
+    </Wrapper>
+  )
+}
