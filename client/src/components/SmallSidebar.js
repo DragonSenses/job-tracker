1aee6366834d51de4a58d5b7b7c179dc24510638
DragonSenses <kendrickfrancisco@gmail.com>
2023-03-22 18:58:21 -0700

    Create Sidebar

---
+++ client/src/components/SmallSidebar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Wrapper from '../assets/wrappers/SmallSidebar.js';
+
+export default function SmallSidebar() {
+  return (
+    <Wrapper>
+      SmallSidebar
+    </Wrapper>
+  )
+}
