147befb50912aa9a2fea7d0406bcf50c5d417e78
DragonSenses <kendrickfrancisco@gmail.com>
2023-03-26 04:27:14 -0700

    Temporarily stop pre save hook

--- models/User.js
+++ models/User.js
@@ -43,8 +43,8 @@ const UserSchema = new Schema({
 });
 
 UserSchema.pre('save', async function(){
-  const salt = await bcryptjs.genSalt(10);
-  this.password = await bcryptjs.hash(this.password, salt);
+  // const salt = await bcryptjs.genSalt(10);
+  // this.password = await bcryptjs.hash(this.password, salt);
 });
 
 UserSchema.methods.createToken = function () {
