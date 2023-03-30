fd4e9abf7ab79b03d5219ed0245e6ab1565a6e2c
DragonSenses <kendrickfrancisco@gmail.com>
2023-03-29 23:17:11 -0700

    Handle Update User state

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -107,15 +107,27 @@ const reducer = (state, action) => {
     };
   }
 
-  if(action.type === UPDATE_USER_BEGIN) {
+  if(action.type === UPDATE_USER_SUCCESS) {
     return {
       ...state,
+      isLoading: false,
+      token: action.payload.token,
+      user: action.payload.user,
+      userLocation: action.payload.location,
+      jobLocation: action.payload.location,
+      showAlert: true,
+      alertType: 'success',
+      alertText: 'User Profile Updated!'
     };
   }
 
-  if(action.type === UPDATE_USER_BEGIN) {
+  if(action.type === UPDATE_USER_ERROR) {
     return {
       ...state,
+      isLoading: false,
+      showAlert: true,
+      alertType: 'danger',
+      alertText: action.payload.msg,
     };
   }
 
