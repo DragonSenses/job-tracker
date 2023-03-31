ce9b1eac89b16563ba7f80743ebaea872cee9f08
DragonSenses <kendrickfrancisco@gmail.com>
2023-03-30 22:05:38 -0700

    Create Job.js

---
+++ models/Job.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import validator from 'validator';
+import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+const { Schema } = mongoose;
+
+const JobSchema = new Schema({
+  
+});
