c818a4268ef0d5de4146d379898d0c85698628f0
DragonSenses <kendrickfrancisco@gmail.com>
2023-04-01 00:53:52 -0700

    Update FormRowSelect component

--- client/src/components/FormRowSelect.js
+++ client/src/components/FormRowSelect.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
-export default function FormRowSelect() {
+export default function FormRowSelect({labelText, name, value, handleChange, list}) {
   return (
-    <div>FormRowSelect</div>
-  )
+    <div className="form-row">
+    <label htmlFor={name} className='form-label'>
+      {labelText || name}
+    </label>
+
+    <select
+      name={name}
+      value={value}
+      onChange={handleChange}
+      className='form-select'
+    >
+      {list.map((itemValue, index) => {
+        return(
+          <option key={index} value={itemValue}>
+            {itemValue}
+          </option>
+        );
+      })}
+    </select>
+  </div> 
+  );
 }
