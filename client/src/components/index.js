31013581f2603b3c3c6f6baa2300c681f83fe9fe
DragonSenses <kendrickfrancisco@gmail.com>
2023-04-08 12:58:44 -0700

    Update index.js

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,17 +1,21 @@
 import Alert from "./Alert";
 import FormRow from "./FormRow";
 import FormRowSelect from "./FormRowSelect";
+import JobsContainer from "./JobsContainer";
 import LargeSidebar from "./LargeSidebar";
 import Logo from "./Logo";
 import Navbar from "./Navbar";
+import SearchContainer from "./SearchContainer";
 import SmallSidebar from "./SmallSidebar";
 
 export { 
   Alert,
   FormRow,
   FormRowSelect,
+  JobsContainer,
   LargeSidebar,
   Logo,
   Navbar,
+  SearchContainer,
   SmallSidebar,
 };
