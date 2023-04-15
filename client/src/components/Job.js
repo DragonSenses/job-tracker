75f98a031ca2132b54ec2c0a9d405405a58ebda3
DragonSenses <kendrickfrancisco@gmail.com>
2023-04-14 23:10:02 -0700

    Use Wrapper in the return

--- client/src/components/Job.js
+++ client/src/components/Job.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import moment from 'moment';
+import { FaLocationArrow, FaBriefcase, FaCalendarAlt } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { useAppContext } from '../context/appContext';
+import Wrapper from '../assets/wrappers/Job';
 
 export default function Job({props}) {
   const { company, createdAt } = props;
@@ -8,10 +12,10 @@ export default function Job({props}) {
   date = date.format('MMM Do, YYYY'); // Apr 14th 23
 
   return (
-    <div>
+    <Wrapper>
       Job
       <h5>{company}</h5>
       <h5>{date}</h5>
-    </div>
+    </Wrapper>
   );
 }
