0266226eb91677a41b63fd74f484d1c22780d506
DragonSenses <kendrickfrancisco@gmail.com>
2023-04-15 23:21:13 -0700

    Update JobInfo.js

--- client/src/components/JobInfo.js
+++ client/src/components/JobInfo.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Wrapper from '../assets/wrappers/JobInfo';
 
 export default function JobInfo(props) {
-  
+  const { icon, text } = props;
+
   return (
     <Wrapper>
-      JobInfo
+      <span className="icon">{icon}</span>
+      <span className="text">{text}</span>
     </Wrapper>
   );
 }
