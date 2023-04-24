b93b36c0f44a489c67d89fa55160375bd7b11196
DragonSenses <kendrickfrancisco@gmail.com>
2023-04-23 23:10:36 -0700

    Update checkPermissions.js

--- utils/checkPermissions.js
+++ utils/checkPermissions.js
@@ -3,9 +3,11 @@ import { UnAuthorizedError } from '../errors/index.js';
 export default function checkPermissions(requestUser, resourceUserId) {
   // Check if user object is admin
 
+  // Check if userId matches to that who created the resource
   if(requestUser.userId === resourceUserId.toString()) {
     return;
   }
 
-  throw new CustomError.UnAuthorizedError("Not authorized to access this route");
+  // Throw error in all other cases
+  throw new UnAuthorizedError("Not authorized to access this route");
 };
