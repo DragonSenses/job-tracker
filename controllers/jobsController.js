7d09c7327b951bb1b4682387d51efb7b04b0890a
DragonSenses <kendrickfrancisco@gmail.com>
2023-04-28 16:01:56 -0700

    Update jobsController.js

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -87,7 +87,7 @@ const deleteJob = async (req, res) => {
 };
 
 const showStats = async (req, res) => {
-  
+
   let stats = await Job.aggregate([
     { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
     { $group: { _id: '$status', count: { $sum: 1 } } },
@@ -99,7 +99,21 @@ const showStats = async (req, res) => {
     return acc;
   }, {});
 
-  res.status(StatusCodes.OK).json({ stats });
+  stats = stats.reduce((acc, curr) => {
+    const { _id: title, count } = curr;
+    acc[title] = count;
+    return acc;
+  }, {});
+
+  const defaultStats = {
+    pending: stats.pending || 0,
+    interview: stats.interview || 0,
+    declined: stats.declined || 0,
+  };
+
+  let monthlyApplications = [];
+  
+  res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
 
 export { createJob, getAllJobs, updateJob, deleteJob, showStats }
