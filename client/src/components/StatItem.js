a99b3cad8590365570ca05e381fb0ed942c9ab58
DragonSenses <kendrickfrancisco@gmail.com>
2023-05-02 19:04:51 -0700

    Update StatItem.js

--- client/src/components/StatItem.js
+++ client/src/components/StatItem.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import Wrapper from '../assets/wrappers/StatItem';
 
-export default function StatItem() {
+export default function StatItem(props) {
+  const {
+    count,
+    title,
+    icon,
+    color,
+    bcg,
+  } = props;
+
   return (
-    <div>StatItem</div>
+    <Wrapper color={color} bcg={bcg}>
+      <header>
+        <span className="count">{count}</span>
+        <div className="icon">{icon}</div>
+      </header>
+      <h5 className="title">{title}</h5>
+    </Wrapper>
   );
 }
