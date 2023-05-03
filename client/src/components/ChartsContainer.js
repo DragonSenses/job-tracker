836b4deda46432ed8b703420c91bc4d624516c1f
DragonSenses <kendrickfrancisco@gmail.com>
2023-05-03 10:14:07 -0700

    Update ChartsContainer.js

--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import { useAppContext } from '../context/appContext';
+import Wrapper from '../assets/wrappers/ChartsContainer';
 
 export default function ChartsContainer() {
+  const { monthlyApplications: data } = useAppContext();
+
   return (
-    <div>ChartsContainer</div>
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+
+      <button type='button' onClick={}>
+
+      </button>
+    </Wrapper>
   );
 }
