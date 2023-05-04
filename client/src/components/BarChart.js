3511eb3bc86dd15ab26d988954c4bf88ca5da2f8
DragonSenses <kendrickfrancisco@gmail.com>
2023-05-04 16:28:43 -0700

    Update BarChart.js

--- client/src/components/BarChart.js
+++ client/src/components/BarChart.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import {
-  BarChart,
   Bar,
-  XAxis,
-  YAxis,
+  BarChart,
   CartesianGrid,
+  ResponsiveContainer,
   Tooltip,
-  ResponsiveContainer
+  XAxis,
+  YAxis
 } from 'recharts';
 
 export default function BarChartComponent({ data }) {
