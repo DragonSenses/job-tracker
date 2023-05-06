e22a761e548e8b941507f944269e781bbedd08c6
DragonSenses <kendrickfrancisco@gmail.com>
2023-05-05 20:18:58 -0700

    Finalize ChartsContainer.js

--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,18 +1,23 @@
 import React, {useState} from 'react';
-
 import { useAppContext } from '../context/appContext';
 import Wrapper from '../assets/wrappers/ChartsContainer';
+import AreaChart from './AreaChart';
+import BarChart from './BarChart';
 
 export default function ChartsContainer() {
+  const [barChart, setBarChart] = useState(true);
   const { monthlyApplications: data } = useAppContext();
 
   return (
     <Wrapper>
       <h4>Monthly Applications</h4>
 
-      <button type='button' onClick={}>
-
+      <button type='button' onClick={() => setBarChart(!barChart)}>
+        {barChart ? 'AreaChart' : 'BarChart'}
       </button>
+
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+
     </Wrapper>
   );
 }
