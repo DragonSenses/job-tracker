fca4afe414d6f8ea8b58a421b46520f2d79922e3
DragonSenses <kendrickfrancisco@gmail.com>
2023-05-05 18:59:33 -0700

    Update AreaChart.js

--- client/src/components/AreaChart.js
+++ client/src/components/AreaChart.js
@@ -10,15 +10,15 @@ import {
   YAxis
 } from 'recharts';
 
-export default function AreaChartComponent() {
+export default function AreaChartComponent({ data }) {
   return (
     <ResponsiveContainer width='100%' height={300}>
-      <AreaChart>
-        <CartesianGrid />
-        <XAxis />
-        <YAxis />
+      <AreaChart data={data} margin={{ top:50 }}>
+        <CartesianGrid stroke="#ccc" strokeDasharray='3 3' />
+        <XAxis dataKey='date' />
+        <YAxis allowDecimals={false} />
         <Tooltip />
-        <Area />
+        <Area type='monotone' dataKey='count' stroke='#2cb1bc' fill='#bef8fd'/>
       </AreaChart>
     </ResponsiveContainer>
   );
