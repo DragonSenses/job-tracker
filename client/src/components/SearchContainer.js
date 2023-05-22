3a4f24232db45d0c055cb9d7eb0a6ff489f70bc3
DragonSenses <kendrickfrancisco@gmail.com>
2023-05-21 21:54:24 -0700

    Create event handler for Search Container

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -17,14 +17,23 @@ export default function SearchContainer() {
     clearFilters,
   } = useAppContext();
 
+  const handleSearch = (e) => {
+    if(isLoading) return;
+    handleChange({ name: e.target.name, value: e.target.value });
+  }
+
   return (
     <Wrapper>
       <form action="" className="form">
         <h4>search form</h4>
         <div className="form-center">
-            <FormRow>
-
-            </FormRow>
+          <FormRow
+            type='text'
+            name='search'
+            value={search}
+            handleChange={handleSearch}
+          >
+          </FormRow>
         </div>
       </form>    
     </Wrapper>
