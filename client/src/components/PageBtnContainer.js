74cd445c73ada6826eaf0153bf496cbc69324ce2
DragonSenses <kendrickfrancisco@gmail.com>
2023-05-25 05:52:46 -0700

    Add imports and functions

--- client/src/components/PageBtnContainer.js
+++ client/src/components/PageBtnContainer.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi';
+import { useAppContext } from '../context/appContext';
+import Wrapper from '../assets/wrappers/PageBtnContainer';
+
 
 export default function PageBtnContainer() {
+
+  const { page, numOfPages } = useAppContext();
+
+  const prevPage = () => {
+    console.log('previous page');
+  };
+  
+  const nextPage = () => {
+    console.log('next page');
+  };
+
   return (
     <div>PageBtnContainer</div>
   );
