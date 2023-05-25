bd996eb18db0b2c869e5c2ea8f7fde533d59a300
DragonSenses <kendrickfrancisco@gmail.com>
2023-05-24 18:57:58 -0700

    Update pagination variables

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -72,13 +72,13 @@ const getAllJobs = async (req, res) => {
   let jobs = await result;
 
   // Pagination Variables
-  const limit = 10;
-  const skip = 1;
+  const page = Number(req.query.page) || 1;
 
-  // Chain operations to modify the query based on the page variables
-  result = result.skip(skip).limit(limit);  
+  const limit = Number(req.query.limit) || 10;
 
-  // Since the query operations are async, we need to await again
+  const skip = (page - 1) * limit;
+
+  // Await jobs filtered out by sort conditions AND processed through pagination
   jobs = await result;
 
   // Respond with 200 and a json containing the jobs, totalJobs, and pages
