c79a52405a4101facbcf843c86ed39e0f2c6cb2a
DragonSenses <kendrickfrancisco@gmail.com>
2023-05-24 19:04:53 -0700

    Add skip and limit to query

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -73,13 +73,16 @@ const getAllJobs = async (req, res) => {
 
   // Pagination Variables
   const page = Number(req.query.page) || 1;
-
   const limit = Number(req.query.limit) || 10;
-
   const skip = (page - 1) * limit;
 
-  // Await jobs filtered out by sort conditions AND processed through pagination
+  // Chain operations to modify the query based on the page variables
+  result = result.skip(skip).limit(limit);  
+
+  // Await filtered jobs processed through pagination
   jobs = await result;
+
+  // Calculate the variables needed to send back as a response
 
   // Respond with 200 and a json containing the jobs, totalJobs, and pages
   res.status(StatusCodes.OK)
