dd6635585383f304d1a88c92c0c4426bf3619f85
DragonSenses <kendrickfrancisco@gmail.com>
2023-06-01 17:22:08 -0700

    Import express-rate-limit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 import { register, login, updateUser } from '../controllers/authController.js';
 import authenticateUser from '../middleware/authenticate.js';
 
+import rateLimiter from 'express-rate-limit';
+
 router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/updateUser').patch(authenticateUser, updateUser);
