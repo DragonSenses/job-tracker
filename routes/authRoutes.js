c0de64bf713a8815a5c7b32dbe8d8af1729f0aeb
DragonSenses <kendrickfrancisco@gmail.com>
2023-06-02 19:34:29 -0700

    Create apiLimiter and apply it

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,8 +6,15 @@ import authenticateUser from '../middleware/authenticate.js';
 
 import rateLimiter from 'express-rate-limit';
 
-router.route('/register').post(register);
-router.route('/login').post(login);
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10, // Limit each IP to 10 requests per `window` (here, per 15 minutes)
+  message:
+		'Too many accounts created from this IP, please try again after 15 minutes',
+});
+
+router.route('/register').post(apiLimiter, register);
+router.route('/login').post(apiLimiter, login);
 router.route('/updateUser').patch(authenticateUser, updateUser);
 
 export default router
