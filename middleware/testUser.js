e0312f3a8589122ca6f425e4a0e3442561fcddc2
DragonSenses <kendrickfrancisco@gmail.com>
2023-06-04 09:52:40 -0700

    Create testUser middleware

---
+++ middleware/testUser.js
@@ -0,0 +1,12 @@
+import { BadRequestError } from '../errors/index.js';
+
+function testUser(req, res, next) {
+  
+  if(req.user.testUser){
+    throw new BadRequestError('Test User. Read Only!');
+  }
+  
+  next();
+};
+
+export default testUser;
