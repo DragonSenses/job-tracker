2ba7e773b98a1bc8631d1d86e6460689a30f0de5
DragonSenses <kendrickfrancisco@gmail.com>
2023-06-06 11:00:17 -0700

    Group Landing with a conditional navigate
    - Uses context to determine if user exist. If so, then navigates them back to the main page
    - Fragment groups together both the styled-component Wrapper and a conditional check for user that renders a Navigate element

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,38 +1,44 @@
 import React from 'react';
+import { Link, Navigate } from 'react-router-dom';
+import { useAppContext } from '../context/appContext';
 import main from '../assets/images/main.svg';
 import Wrapper from '../assets/wrappers/LandingPage';
 import Logo from '../components/Logo';
-import { Link } from 'react-router-dom';
 
 function Landing() {
+  const { user } = useAppContext();
   return (
-    <Wrapper>
-      <nav>
-        <Logo />
-      </nav>
-      <div className="container page">
+    <>
+      {user && <Navigate to='/' />}
+      
+      <Wrapper>
+        <nav>
+          <Logo />
+        </nav>
+        <div className="container page">
 
-        <div className="info">
-          <h1>Job <span>Tracking</span> App</h1>
-          <h4>Track and manage all your job applications in one place.</h4>
-          <p>
-            Are you also feeling lost as a job seeker? I created this app to ease 
-            the process of applying. To give you that organization and certainty fraught
-            with a time where there is little to none. Keep sending out applications.
-            I know it is discouraging, but you have to bump the number of applications up.
-            You are not alone in this struggle, I'm also in the same process.
-            I hope this app will prevent you from growing too despondent. <strong>"If I can ease one
-            life the aching, I shall not live in vain" - Emily Dickinson</strong>
-          </p>
-          <Link to='/register' className='btn btn-hero'>
-            Login/Register
-          </Link>
+          <div className="info">
+            <h1>Job <span>Tracking</span> App</h1>
+            <h4>Track and manage all your job applications in one place.</h4>
+            <p>
+              Are you also feeling lost as a job seeker? I created this app to ease 
+              the process of applying. To give you that organization and certainty fraught
+              with a time where there is little to none. Keep sending out applications.
+              I know it is discouraging, but you have to bump the number of applications up.
+              You are not alone in this struggle, I'm also in the same process.
+              I hope this app will prevent you from growing too despondent. <strong>"If I can ease one
+              life the aching, I shall not live in vain" - Emily Dickinson</strong>
+            </p>
+            <Link to='/register' className='btn btn-hero'>
+              Login/Register
+            </Link>
+          </div>
+
+          <img src={main} alt="job hunt" className='img main-img'></img>
+
         </div>
-
-        <img src={main} alt="job hunt" className='img main-img'></img>
-
-      </div>
-    </Wrapper>
+      </Wrapper>
+    </>
   );
 }
 
