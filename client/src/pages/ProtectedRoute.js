b360000bb22b18d0eb11c2d11310fff578479ceb
DragonSenses <kendrickfrancisco@gmail.com>
2023-06-06 10:51:03 -0700

    Use context to conditionally render user loading

--- client/src/pages/ProtectedRoute.js
+++ client/src/pages/ProtectedRoute.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAppContext } from '../context/appContext';
+import Loading from '../components/Loading';
 
 export default function ProtectedRoute({ children }) {
-  const { user } = useAppContext();
+  const { user, userLoading } = useAppContext();
+
+  if(userLoading) {
+    return <Loading />;
+  }
+
   if(!user){
     return <Navigate to='/landing' />
   }
+
   return (
     children
   );
-}
+};
